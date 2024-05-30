261165282d1484a829423a3dc315a11f767effc8
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-30 16:36:43 +0200

    set an array to fill the todolist

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 const { createApp } = Vue
 
 createApp({
-data() {
-    return {
-    
+    data() {
+        return {
+            toDolist: [
+                {
+                    content: `Go to the store and buy the ingredient for the cake`,
+                    done: false
+                },
+                {
+                    content: `Clean the car`,
+                    done: false
+                },
+                {
+                    content: `Bake the cake for the party`,
+                    done: false
+                }
+            ]
+        }
     }
-}
 }).mount('#app')
